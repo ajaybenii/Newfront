c5305f5474f66abf8324b1536d4a569da6671bd6
Ajay Beniwal <85953534+ajaybenii@users.noreply.github.com>
2021-06-25 15:22:08 +0530

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,7 +5,7 @@ import React from 'react'
 
 function TodoItem(props) {
     const deleteTodoHandler = (title) => {
-    axios.delete(`http://localhost:8000/api/todo/${title}`)
+    axios.delete(`https://new-bg.herokuapp.com/api/todo${title}`)
         .then(res => console.log(res.data)) }
   
     return (
